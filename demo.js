899a2ccba16df93a5391ebb5da32315c73cbc2b8
Gregor Martynus <gregor@martynus.net>
2019-11-22 15:02:36 -0800

    Initial demo

---
+++ demo.js
@@ -0,0 +1,6 @@
+// see https://github.com/prettier/prettier/blob/master/CHANGELOG.md#1171
+function f() {
+  if (true) {
+    call("this line more than 79 chars", "long", "it should", "be split into multiple lines");
+  }
+}
