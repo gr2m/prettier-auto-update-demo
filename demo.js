abdb1da6557170c35665c2d016acbd27431d6b49
Create or Update Pull Request Action <create-or-update-pull-request@users.noreply.github.com>
2019-11-23 00:28:33 +0000

    style: prettier

--- demo.js
+++ demo.js
@@ -1,6 +1,11 @@
 // see https://github.com/prettier/prettier/blob/master/CHANGELOG.md#1171
 function f() {
   if (true) {
-    call("this line more than 79 chars", "long", "it should", "be split into multiple lines");
+    call(
+      "this line more than 79 chars",
+      "long",
+      "it should",
+      "be split into multiple lines"
+    );
   }
 }
